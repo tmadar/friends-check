ea75f9cc142545d84cf7f2d36bba1f9e2d684c0d
unknown <travismadar@gmail.com>
2023-08-19 10:00:52 -0700

    do GET and wip POST

--- README.md
+++ README.md
@@ -2,6 +2,38 @@
 
 ## Starting application
 
-Build the docker contaienr. Run `docker build . -t 'friends-check'`
+Build the docker container. Run `docker build . -t 'friends-check'`
 
 Run the tagged docker container that was just built. Run `docker run -p 3000:3000 -d friends-check`
+
+## Data
+
+Pre-generated list can be found in `src/friendlist.json`
+
+## Endpoints
+
+### GET '/user/:userId'
+
+Gets the user based off of their ID
+
+Example responses:
+```json
+{
+    "success": true,
+    "data": {
+        "id": 1,
+        "name": "Ron Weasley",
+        "friends": [
+            0,
+            2
+        ]
+    }
+}
+```
+
+```json
+{
+    "error": true,
+    "message": "No user found with that user ID!"
+}
+```
--- src/server.js
+++ src/server.js
@@ -1,9 +1,49 @@
+const friendlist = require('./friendlist.json');
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const isValidCreatePayload = (data) => {
+  return data !== null && typeof data === 'object'
+    && data.id !== null && !isNaN(parseInt(data.id))
+    && data.name !== null && typeof data.name === 'string'
+    && data.friends !== null
+    && data.friends.every((entry) => {
+      return typeof entry === 'number';
+    })
+};
+
+app.get('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  const user = friendlist.data.find((user) => user.id == userId);
+
+  if (!user) {
+    return res.status(404).send({ error: true, message: 'No user found with that user ID!'});
+  }
+
+  res.status(200).send({ success: true, data: user });
+})
+
+
+app.post('/user', (req, res) => {
+  const newUserData = req.body
+
+  if (!isValidCreatePayload(newUserData)) {
+    return res.status(400).send({ error: true, message: 'Invalid data provided!' });
+  }
+
+  const { id: userId } = newUserData;
+
+  const user = friendlist.data.find((user) => user.id == userId);
+
+  if (user) {
+    return res.status(409).send({ error: true, message: 'User already exists with that ID!' });
+  }
+
+  friendlist.data.push(newUserData);
+
+  res.status(200).send({ success: true });
 })
 
 app.listen(port, () => {
